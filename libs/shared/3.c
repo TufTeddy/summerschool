389501469175f36ee10d88ac9f1639ca52e37e41
tufteddy <godisunstoppable@gmail.com>
2018-07-12 21:50:34 +0700

    Test Shared and Archive librarys

---
+++ libs/shared/3.c
@@ -0,0 +1,37 @@
+#include "func.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int choice;
+	while (1){
+		printf ("\n\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n0.Exit\n");
+		scanf ("%d", &choice);
+		switch(choice){
+			case 1:
+				clear();
+				input();
+				add();
+				break;
+			case 2:
+				clear();
+				input();
+				sub();
+				break;
+			case 3:
+				clear();
+				input();
+				mult();
+				break;
+			case 4:
+				clear();
+				input();
+				divis();
+				break;
+			case 0: 
+				exit(1);
+		}
+	}
+	
+	return 0;
+}
---
+++ libs/shared/func.h
@@ -0,0 +1,8 @@
+#define clear() printf("\033[H\033[J")
+
+void input ();
+void print (int, int);
+void add ();
+void sub ();
+void mult ();
+void divis ();
